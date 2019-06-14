dce01228cdc5d1511363e862dbb4ee2942305611
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2019-06-14 14:59:44 +0300

    Setup Carthage

---
+++ Realm+JSON/Realm_JSON.h
@@ -0,0 +1,19 @@
+//
+//  Realm_JSON.h
+//  Realm+JSON
+//
+//  Created by Viktoras Laukevicius on 14/06/2019.
+//  Copyright © 2019 io.github.viktorasl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Realm_JSON.
+FOUNDATION_EXPORT double Realm_JSONVersionNumber;
+
+//! Project version string for Realm_JSON.
+FOUNDATION_EXPORT const unsigned char Realm_JSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Realm_JSON/PublicHeader.h>
+
+
